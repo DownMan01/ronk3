47f11aabbbf224a787826de87e3cd98451446258
J <98541904+DownMan01@users.noreply.github.com>
2025-08-20 18:22:39 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,8 +61,8 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32"/>
         <link rel="apple-touch-icon" href="/favicon.png" />
-        <meta name="amobile-web-app-capable" content="yes" />
-        <meta name="google-adsense-account" content="ca-pub-6639387700552171">
+        <meta name="amobile-web-app-capable" content="yes"/>
+        <meta name="google-adsense-account" content="ca-pub-6639387700552171"/>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
