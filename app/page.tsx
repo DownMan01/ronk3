f405959b3d29354f86d4e6db4995ad7928b782a7
J <98541904+DownMan01@users.noreply.github.com>
2025-08-21 20:11:36 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ const FARMER_TYPES = {
 
 const FARMING_FORMULA = {
   totalDailyReward: 148408.65,
-  totalNetworkYield: 1933835,
+  totalNetworkYield: 1980315,
 }
 
 export default function InvestmentCalculator() {
